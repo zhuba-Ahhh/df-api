313be511224f1d5f74e43a8da201f58201a18b27
zhuba-Ahhh <3477826311@qq.com>
2024-11-15 10:40:43 +0800

    chore: :hammer: 图片

---
+++ src/common/json/arrow.ts
@@ -0,0 +1,6 @@
+const arrow = [
+  'https://game.gtimg.cn/images/dfm/cp/a20240807community/tool/select_arrow.png',
+  'https://game.gtimg.cn/images/dfm/cp/a20240807community/armor/select_arrow2.png',
+];
+
+export { arrow };
---
+++ src/common/json/rarity.ts
@@ -0,0 +1,10 @@
+const rarityIcons = [
+  'https://game.gtimg.cn/images/dfm/cp/a20240807community/icon_rarity_1.png',
+  'https://game.gtimg.cn/images/dfm/cp/a20240807community/icon_rarity_2.png',
+  'https://game.gtimg.cn/images/dfm/cp/a20240807community/icon_rarity_3.png',
+  'https://game.gtimg.cn/images/dfm/cp/a20240807community/icon_rarity_4.png',
+  'https://game.gtimg.cn/images/dfm/cp/a20240807community/icon_rarity_5.png',
+  'https://game.gtimg.cn/images/dfm/cp/a20240807community/icon_rarity_6.png',
+];
+
+export { rarityIcons };
