1c4ce6bdce273147e3bffc670e0b48dd1bee60ce
zhuba-Ahhh <3477826311@qq.com>
2025-02-08 18:05:46 +0800

    feat: :sparkles: ck

--- src/info/info.controller.ts
+++ src/info/info.controller.ts
@@ -5,8 +5,8 @@ import { InfoService } from './info.service';
 export class InfoController {
   constructor(private readonly infoService: InfoService) {}
   @Get('getInfo')
-  async getInfo(@Query('page') page: string) {
-    const data = await this.infoService.getInfo(page);
+  async getInfo(@Query('page') page: string, @Query('ck') ck: string) {
+    const data = await this.infoService.getInfo(page, ck);
     return { code: 1, data }; // 封装返回格式
   }
 }
