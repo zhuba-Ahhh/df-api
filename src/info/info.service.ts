c1c54f9eaf0ff83a0b2a891301b25da3bc46e913
zhuba-Ahhh <3477826311@qq.com>
2025-03-04 20:03:25 +0800

    feat: :sparkles: 数据储存

--- src/info/info.service.ts
+++ src/info/info.service.ts
@@ -1,7 +1,5 @@
 import { Injectable } from '@nestjs/common';
 import { Cron } from '@nestjs/schedule';
-import * as fs from 'fs-extra';
-import * as path from 'path';
 import { ckOptions, AssetData } from '../common/const';
 
 import axios from 'axios';
@@ -14,15 +12,7 @@ export class InfoService {
     'uin=o3477826311; RK=b59FxoIHym; ptcz=3777785d342a528eddf82ba651955ccb98caacf6f435c3c384528525cd0c6484; pgv_pvid=7196508015; eas_sid=21A7g2d2Y5t0Q447b1g8X9L8K3; lplqqcomrouteLine=a20240115lpl_a20240115lpl_a20240115lpl; rv2=80502F3A38E6C3F70F730D59DE0147F331CC33FFFFC02123C4; property20=946F9D3A1B6588D45437A57AC4230CFBE51F1C96D894E540A3D998831349302B87A833D7E96FCEEB; dfqqcomrouteLine=a20240729directory_index_a20240729directory_index_a20240729directory_index_pc; ied_qq=o3477826311; pgv_info=ssid; skey=@JBz9L47JS; iegams_milo_proxylogin_qc=101491592_%24%24_6299DAAC80EA915F34FDA0BE5A6B2635_%24%24_514422781D6FF22FFD04013B58741788; refresh_token=; expires_time=; acctype=qc; openid=6299DAAC80EA915F34FDA0BE5A6B2635; access_token=514422781D6FF22FFD04013B58741788; appid=101491592; ieg_ams_token=; ieg_ams_session_token=; ieg_ams_token_time=; ieg_ams_sign=';
   private readonly targetUrl = 'https://comm.ams.game.qq.com/ide/';
 
-  private readonly dataFilePath = path.join(__dirname, '../data/assets.json');
-  constructor() {
-    // 确保数据目录存在
-    fs.ensureDirSync(path.dirname(this.dataFilePath));
-    // 如果文件不存在，创建一个空的 JSON 文件
-    if (!fs.pathExistsSync(this.dataFilePath)) {
-      fs.writeJsonSync(this.dataFilePath, [], { spaces: 2 });
-    }
-  }
+  private readonly ASSETS_FILENAME = 'df-assets.json';
 
   private getHeaders(ck?: string) {
     return {
@@ -127,13 +117,12 @@ export class InfoService {
     try {
       const timestamp = new Date().toISOString();
 
-      // 读取现有数据
+      // 读取固定文件的数据
       let existingData: AssetData[] = [];
       try {
-        const { blobs } = await list();
+        const { blobs } = await list({ prefix: this.ASSETS_FILENAME });
         if (blobs.length > 0) {
-          const latestBlob = blobs[blobs.length - 1];
-          const response = await fetch(latestBlob.url);
+          const response = await fetch(blobs[0].url);
           existingData = await response.json();
         }
       } catch (error) {
@@ -162,33 +151,31 @@ export class InfoService {
         }),
       );
 
-      // 上传到 Vercel Blob
+      // 始终更新同一个文件
       try {
-        const filename = `assets-${timestamp}.json`;
         const blob = new Blob([JSON.stringify(newData, null, 2)], {
           type: 'application/json',
         });
 
-        const { url } = await put(filename, blob, {
+        const { url } = await put(this.ASSETS_FILENAME, blob, {
           access: 'public',
         });
 
-        console.log('数据上传成功，访问地址:', url);
+        console.log('数据更新成功，访问地址:', url);
       } catch (uploadError) {
-        console.error('上传到 Vercel Blob 失败:', uploadError);
+        console.error('更新 Vercel Blob 失败:', uploadError);
       }
     } catch (error) {
       console.error('Error updating assets data:', error);
     }
   }
 
-  // 获取本地数据的方法
+  // 修改获取数据的方法
   async getLocalAssets() {
     try {
-      const { blobs } = await list();
+      const { blobs } = await list({ prefix: this.ASSETS_FILENAME });
       if (blobs.length > 0) {
-        const latestBlob = blobs[blobs.length - 1];
-        const response = await fetch(latestBlob.url);
+        const response = await fetch(blobs[0].url);
         return await response.json();
       }
       return null;
